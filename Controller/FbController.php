17d85acc05b904a01e35215c6c88784d05704dfe
Manuel Sanchís <manuel@edemy.es>
2015-06-16 19:00:05 +0200

    refactoring

--- Controller/FbController.php
+++ Controller/FbController.php
@@ -18,8 +18,8 @@ class FbController extends BaseController
             'edemy_facebook_login'      => array('onFacebookLogin', 0),
             'edemy_meta_module'         => array('onMetaModule', 0),
             'edemy_precontent_module'   => array('onPreContentModule', 0),
-            'edemy_facebook_sorteo'     => array('onSorteo', 0),
-
+            'edemy_facebook_promo'     => array('onPromo', 0),
+            'edemy_facebook_privacy'     => array('onFacebookPrivacy', 0),
         ));
     }
 
@@ -83,9 +83,14 @@ class FbController extends BaseController
         return $fb_count[0]->like_count;
     }
 
+    public function onFacebookPrivacy(ContentEvent $event)
+    {
+        $this->addEventModule($event, 'templates/fbPrivacy');
+    }
+
     public function onFacebookLogin(ContentEvent $event)
     {
-        FB\FacebookSession::setDefaultApplication('__app_id__', 'token');
+        FB\FacebookSession::setDefaultApplication('882199081823442', '3550bed57545c7240765f0b3967d1639');
         $redirectUrl = $this->get('router')->generate('edemy_facebook_login', array(), true);
         $helper = new FB\FacebookRedirectLoginHelper($redirectUrl);
         try {
@@ -120,41 +125,86 @@ class FbController extends BaseController
         }
     }
 
-    public function onSorteo(ContentEvent $event)
+    public function onPromo(ContentEvent $event)
     {
-        FB\FacebookSession::setDefaultApplication('', 'token');
-        $redirectUrl = $this->get('router')->generate('edemy_facebook_login', array(), true);
+        $request = $this->get('request_stack')->getCurrentRequest();
+        $_route = $request->attributes->get('id');
+
+        $required_scope     = 'public_profile, publish_actions, email'; //Permissions required
+        $redirectUrl = $this->get('router')->generate('edemy_facebook_promo', array(), true);
+
+        FB\FacebookSession::setDefaultApplication('882199081823442', '3550bed57545c7240765f0b3967d1639');
         $helper = new FB\FacebookRedirectLoginHelper($redirectUrl);
+
         try {
-            if($this->get('session')->get('facebook_token')) {
-                $session = new FB\FacebookSession($this->get('session')->get('facebook_token'));
-            } else {
-                $session = $helper->getSessionFromRedirect();
-            }
-        } catch(FB\FacebookRequestException $ex) {
-        } catch(\Exception $ex) {
+            $session = $helper->getSessionFromRedirect();
+        } catch(FacebookRequestException $ex) {
+            // When Facebook returns an error
+        } catch(Exception $ex) {
+            // When validation fails or other local issues
         }
-        if (isset($session)) {
-            $this->get('session')->set('facebook_token', $session->getToken());
-            // graph api request for user data
-            $request = new FB\FacebookRequest( $session, 'GET', '/me' );
-            $response = $request->execute();
-            // get response
-            $graphObject = $response->getGraphObject();
-            $fbid = $graphObject->getProperty('id');              // To Get Facebook ID
-            $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
-            $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
-            $_SESSION['FBID'] = $fbid;
-            $_SESSION['FULLNAME'] = $fbfullname;
-            $_SESSION['EMAIL'] =  $femail;
-            //checkuser($fuid,$ffname,$femail);
-            //header("Location: index.php");
-            $this->addEventModule($event, 'templates/testFb', array(
-                'fbfullname' => $fbfullname,
+
+        if (isset($session) ) {
+            $user_profile = (new FB\FacebookRequest($session, 'GET', '/me'))->execute()->getGraphObject(FB\GraphUser::className());
+            //$graphObject->getProperty("email");
+            echo print_r($user_profile);
+
+            $this->addEventModule($event, 'templates/fbPromo', array(
+                'id' => $user_profile->getProperty('id'),
+                'name' => $user_profile->getProperty('name'),
+                'first_name' => $user_profile->getProperty('first_name'),
+                'last_name' => $user_profile->getProperty('last_name'),
+                'middle_name' => $user_profile->getMiddleName(),
+                'link' => $user_profile->getLink(),
+                'birthday' => $user_profile->getBirthday(),
+                'location' => $user_profile->getLocation(),
+                'email' => $user_profile->getEmail(),
+                'gender' => $user_profile->getGender(),
+//                'verified' => $user_profile->getVerified(),
             ));
         } else {
-            $event->addEventModule($event, 'templates/fbSorteoPuraAlegria');
+            $login_url = $helper->getLoginUrl( array( 'scope' => $required_scope ) );
+            $this->addEventModule($event, 'templates/fbPromo', array(
+                'login_url' => $login_url,
+            ));
         }
+
+
+//            $request = new FB\FacebookRequest($session,'POST','/me/feed', array ('message' => 'hola', 'scope' => 'publish_actions',));
+//            $response = $request->execute();
+//            $graphObject = $response->getGraphObject();
+
+
+//        try {
+//            if($this->get('session')->get('facebook_token')) {
+//                $session = new FB\FacebookSession($this->get('session')->get('facebook_token'));
+//            } else {
+//                $session = $helper->getSessionFromRedirect();
+//            }
+//        } catch(FB\FacebookRequestException $ex) {
+//        } catch(\Exception $ex) {
+//        }
+//        if (isset($session)) {
+//            $this->get('session')->set('facebook_token', $session->getToken());
+//            // graph api request for user data
+//            $request = new FB\FacebookRequest( $session, 'GET', '/me' );
+//            $response = $request->execute();
+//            // get response
+//            $graphObject = $response->getGraphObject();
+//            $fbid = $graphObject->getProperty('id');              // To Get Facebook ID
+//            $fbfullname = $graphObject->getProperty('name'); // To Get Facebook full name
+//            $femail = $graphObject->getProperty('email');    // To Get Facebook email ID
+//            $_SESSION['FBID'] = $fbid;
+//            $_SESSION['FULLNAME'] = $fbfullname;
+//            $_SESSION['EMAIL'] =  $femail;
+//            //checkuser($fuid,$ffname,$femail);
+//            //header("Location: index.php");
+//            $this->addEventModule($event, 'templates/testFb', array(
+//                'fbfullname' => $fbfullname,
+//            ));
+//        } else {
+//        }
+//        $this->addEventModule($event, 'templates/fbSorteoPuraAlegria');
     }
 
     public function onFacebookLogin2(ContentEvent $event)
