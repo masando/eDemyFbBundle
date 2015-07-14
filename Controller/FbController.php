28d3103c0be9f51004cc1eb84165c15cb6960626
Manuel Sanchís <msanchis@edemy.es>
2015-07-14 23:51:27 +0200

    beta

--- Controller/FbController.php
+++ Controller/FbController.php
@@ -27,6 +27,7 @@ class FbController extends BaseController
 
     public function likesAction()
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN', null, 'No tienes permisos para acceder a este recurso!');
         //$input = new ArgvInput(array());
         //$output = new BufferedOutput();
         //$command = $this->get("edemy.getlikes");
